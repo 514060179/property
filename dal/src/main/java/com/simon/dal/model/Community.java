89af49e2349574ff1933d01c6e7507acbcb24d93
fengtianying <514060179@qq.com>
2018-12-08 14:49:59 +0800

    model调整

--- dal/src/main/java/com/simon/dal/model/Community.java
+++ dal/src/main/java/com/simon/dal/model/Community.java
@@ -27,6 +27,42 @@ public class Community {
     @ApiModelProperty("社区全址")
     private String communityAddress;
 
+    @ApiModelProperty("总面积(平方米)")
+    private BigDecimal communityArea;
+
+    @ApiModelProperty("建筑面积")
+    private BigDecimal communityBuildingArea;
+
+    @ApiModelProperty("道路面积")
+    private BigDecimal communityRoadArea;
+
+    @ApiModelProperty("绿化面积")
+    private BigDecimal communityGreenarea;
+
+    @ApiModelProperty("公用面积")
+    private BigDecimal communityCommonArea;
+
+    @ApiModelProperty("车库面积")
+    private BigDecimal communityGarageArea;
+
+    @ApiModelProperty("车位数量")
+    private Integer communityGarageCount;
+
+    @ApiModelProperty("房间总数")
+    private Integer communityRoomCount;
+
+    @ApiModelProperty("联系人")
+    private String communityContacts;
+
+    @ApiModelProperty("联系人号码")
+    private String communityContactsPhone;
+
+    @ApiModelProperty("备注")
+    private String communityRemark;
+
+    @ApiModelProperty("是否删除")
+    private Boolean communityDeleted;
+
     @ApiModelProperty("管理类型(0普通管理1综合管理)")
     private String communityManagementType;
 
@@ -80,6 +116,102 @@ public class Community {
 
     public void setCommunityAddress(String communityAddress) {
         this.communityAddress = communityAddress == null ? null : communityAddress.trim();
+    }
+
+    public BigDecimal getCommunityArea() {
+        return communityArea;
+    }
+
+    public void setCommunityArea(BigDecimal communityArea) {
+        this.communityArea = communityArea;
+    }
+
+    public BigDecimal getCommunityBuildingArea() {
+        return communityBuildingArea;
+    }
+
+    public void setCommunityBuildingArea(BigDecimal communityBuildingArea) {
+        this.communityBuildingArea = communityBuildingArea;
+    }
+
+    public BigDecimal getCommunityRoadArea() {
+        return communityRoadArea;
+    }
+
+    public void setCommunityRoadArea(BigDecimal communityRoadArea) {
+        this.communityRoadArea = communityRoadArea;
+    }
+
+    public BigDecimal getCommunityGreenarea() {
+        return communityGreenarea;
+    }
+
+    public void setCommunityGreenarea(BigDecimal communityGreenarea) {
+        this.communityGreenarea = communityGreenarea;
+    }
+
+    public BigDecimal getCommunityCommonArea() {
+        return communityCommonArea;
+    }
+
+    public void setCommunityCommonArea(BigDecimal communityCommonArea) {
+        this.communityCommonArea = communityCommonArea;
+    }
+
+    public BigDecimal getCommunityGarageArea() {
+        return communityGarageArea;
+    }
+
+    public void setCommunityGarageArea(BigDecimal communityGarageArea) {
+        this.communityGarageArea = communityGarageArea;
+    }
+
+    public Integer getCommunityGarageCount() {
+        return communityGarageCount;
+    }
+
+    public void setCommunityGarageCount(Integer communityGarageCount) {
+        this.communityGarageCount = communityGarageCount;
+    }
+
+    public Integer getCommunityRoomCount() {
+        return communityRoomCount;
+    }
+
+    public void setCommunityRoomCount(Integer communityRoomCount) {
+        this.communityRoomCount = communityRoomCount;
+    }
+
+    public String getCommunityContacts() {
+        return communityContacts;
+    }
+
+    public void setCommunityContacts(String communityContacts) {
+        this.communityContacts = communityContacts;
+    }
+
+    public String getCommunityContactsPhone() {
+        return communityContactsPhone;
+    }
+
+    public void setCommunityContactsPhone(String communityContactsPhone) {
+        this.communityContactsPhone = communityContactsPhone;
+    }
+
+    public String getCommunityRemark() {
+        return communityRemark;
+    }
+
+    public void setCommunityRemark(String communityRemark) {
+        this.communityRemark = communityRemark;
+    }
+
+    public Boolean getCommunityDeleted() {
+        return communityDeleted;
+    }
+
+    public void setCommunityDeleted(Boolean communityDeleted) {
+        this.communityDeleted = communityDeleted;
     }
 
     public String getCommunityManagementType() {
