b92d376ea514e34cb18e34844477214a30531d78
fengtianying <514060179@qq.com>
2018-12-11 17:27:22 +0800

    时间格式化，用户管理接口

---
+++ backstage/src/main/java/com/simon/backstage/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.simon.backstage.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.simon.backstage.domain.msg.BaseQueryParam;
+import com.simon.backstage.domain.msg.ReturnMsg;
+import com.simon.backstage.service.UserService;
+import com.simon.dal.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author fengtianying
+ * @date 2018/12/11 14:31
+ */
+@RestController
+@RequestMapping("/back/user")
+@Api(value = "UserController", description = "住户")
+public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("add")
+    @ApiOperation("添加住户")
+    public ReturnMsg<User> add(@RequestBody User unit){
+        return ReturnMsg.success();
+    }
+
+    @PostMapping("upd")
+    @ApiOperation("修改住户")
+    public ReturnMsg<User> upd(@RequestBody User unit){
+        return ReturnMsg.success();
+    }
+
+    @GetMapping("del")
+    @ApiOperation("删除住户")
+    public ReturnMsg<User> del(@RequestParam String userId){
+        return ReturnMsg.success();
+    }
+
+    @GetMapping("list")
+    @ApiOperation("住户列表")
+    public ReturnMsg<PageInfo<User>> list(BaseQueryParam baseQueryParam){
+//        return ReturnMsg.success(userService.findOne(1L));
+        return null;
+    }
+}
