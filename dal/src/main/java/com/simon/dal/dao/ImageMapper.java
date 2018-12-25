043d032a07251e16b6b9cf7e6b34b0716012b0c9
fengtianyong <120212485@qq.com>
2018-12-25 13:51:10 +0800

    去除xml没实现的方法

--- dal/src/main/java/com/simon/dal/dao/ImageMapper.java
+++ dal/src/main/java/com/simon/dal/dao/ImageMapper.java
@@ -1,18 +1,12 @@
 package com.simon.dal.dao;
 
+import java.util.List;
+
 import com.simon.dal.model.Image;
 
 public interface ImageMapper {
-    int deleteByPrimaryKey(String imageId);
-
-    int insert(Image record);
-
+	
     int insertSelective(Image record);
 
-    Image selectByPrimaryKey(String imageId);
-
-    int updateByPrimaryKeySelective(Image record);
-
-    int updateByPrimaryKey(Image record);
-
+	int insertBatch(List<Image> list);
 }
