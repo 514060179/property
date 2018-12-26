3a55c88755f7f2da059e7311fb8ba5e08ead953d
fengtianyong <120212485@qq.com>
2018-12-26 16:21:16 +0800

    调整

--- sql/modify(user_role)_change_column(user_id).sql
+++ sql/modify(user_role)_change_column(user_id).sql
@@ -1,2 +1,2 @@
-ALTER TABLE `bms`.`user_role`
-	ADD COLUMN `manager` VARCHAR(50) NOT NULL COMMENT '管理员id' AFTER `user_id`;
+ALTER TABLE `bms`.`user_role`   
+  CHANGE `user_id` `manager_id` VARCHAR(50) CHARSET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '用户id';
