11b6509113afac675dde73c179f120a68e4db791
fengtianying <514060179@qq.com>
2019-01-15 09:23:08 +0800

    Type类提交

---
+++ dal/src/main/java/com/simon/dal/constant/Type.java
@@ -0,0 +1,28 @@
+package com.simon.dal.constant;
+
+/**
+ * @author fengtianying
+ * @date 2019/1/14 11:14
+ */
+public class Type {
+
+    /**
+     * 其他文件
+     */
+    public static final int IMAGE_TYPE_OTHER = 3;
+
+    /**
+     * 公告图片
+     */
+    public static final int IMAGE_TYPE_NOTICE = 2;
+
+    /**
+     * 场所图片
+     */
+    public static final int IMAGE_TYPE_PLACE = 1;
+
+    /**
+     * 投诉/报修图片
+     */
+    public static final int IMAGE_TYPE_COMPLAIN = 0;
+}
