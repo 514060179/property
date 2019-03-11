2b879c570c04becce70847a54c3ac144635892a3
Administrator <514060179@qq.com>
2019-03-11 20:11:36 +0800

    推送

---
+++ dal/src/main/java/com/simon/dal/util/JiGuangPush.java
@@ -0,0 +1,33 @@
+package com.simon.dal.util;
+
+//import cn.jiguang.common.ClientConfig;
+//import cn.jiguang.common.resp.APIConnectionException;
+//import cn.jiguang.common.resp.APIRequestException;
+//import cn.jpush.api.JPushClient;
+//import cn.jpush.api.push.PushResult;
+//import cn.jpush.api.push.model.PushPayload;
+
+public class JiGuangPush {
+
+
+
+
+//    public static void pushAllDevice(){
+//        JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY, null, ClientConfig.getInstance());
+//
+//        // For push, all you need do is to build PushPayload object.
+//        PushPayload payload = buildPushObject_all_all_alert();
+//
+//        try {
+//            PushResult result = jpushClient.sendPush(payload);
+//        } catch (APIConnectionException e1) {
+//            e1.printStackTrace();
+//
+//        } catch (APIRequestException e2) {
+//            e2.printStackTrace();
+//        }
+//    }
+//    public static PushPayload buildPushObject_all_all_alert() {
+//        return PushPayload.alertAll(ALERT);
+//    }
+}
