e3e29172bbdd0c80378131bca3e40177277c84da
simon <514060179@qq.com>
2019-03-26 00:16:43 +0800

    场所记录详情

--- dal/src/main/java/com/simon/dal/dao/ImageMapper.java
+++ dal/src/main/java/com/simon/dal/dao/ImageMapper.java
@@ -3,10 +3,13 @@ package com.simon.dal.dao;
 import java.util.List;
 
 import com.simon.dal.model.Images;
+import org.apache.ibatis.annotations.Param;
 
 public interface ImageMapper {
 	
     int insertSelective(Images record);
 
 	int insertBatch(List<Images> list);
+
+	List<Images> findListById(@Param("id") String id, @Param("type")int type);
 }
