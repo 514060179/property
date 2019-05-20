d7c76de7a325f4031683d0e46e4b503464892aff
fengtianying <514060179@qq.com>
2019-05-20 14:11:27 +0800

    统计返回model调整

--- backstage/src/main/java/com/simon/backstage/controller/PlaceRecordController.java
+++ backstage/src/main/java/com/simon/backstage/controller/PlaceRecordController.java
@@ -8,6 +8,7 @@ import com.simon.backstage.domain.vo.PlaceRecordQueryParam;
 import com.simon.backstage.service.PlaceRecordService;
 import com.simon.backstage.util.ClaimsUtil;
 import com.simon.dal.model.PlaceRecord;
+import com.simon.dal.vo.PlaceRecordStatisData;
 import com.simon.dal.vo.PlaceRecordStatisQuery;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -16,6 +17,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @author fengtianying
@@ -63,7 +65,7 @@ public class PlaceRecordController {
 
     @GetMapping("statis")
     @ApiOperation("场所使用统计")
-    public ReturnMsg<PageInfo<PlaceRecord>> statis(PlaceRecordStatisQuery placeRecordStatisQuery, HttpServletRequest request){
+    public ReturnMsg<List<PlaceRecordStatisData>> statis(PlaceRecordStatisQuery placeRecordStatisQuery, HttpServletRequest request){
         String communityId = ClaimsUtil.getCommunityId(request);
         if(!StringUtils.isEmpty(communityId)){
             placeRecordStatisQuery.setCommunityId(communityId);
